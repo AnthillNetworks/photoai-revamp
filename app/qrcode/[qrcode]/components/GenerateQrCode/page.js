7b21ceeff399b12cc68e8eece47af8dfdac49e16
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-05-30 16:27:20 +0530

    QrCode ..

---
+++ app/qrcode/[qrcode]/components/GenerateQrCode/page.js
@@ -0,0 +1,51 @@
+'use client'
+import React from 'react';
+import Styles from "./page.module.css"
+import { QRCodeSVG } from 'qrcode.react';
+
+function QrCodeGen({EventName}) {
+  return (
+    <div className={Styles.QrCodeGen}>
+            hii
+            {/* <div>
+                <div style={{ color: 'black', fontSize: '20px' }}>Scan QR code to upload your selfie</div>
+                <div ref={qrCodeRef} style={{ boxShadow: '0px 12px 30px 0px rgba(0, 0, 0, 0.8)' }}>
+                    <QRCodeSVG value={`${process.env.NEXT_PUBLIC_WEB_APP_BASE_URL}/upload/${EventName}`} style={{width:"18em",height:"18em",margin:"1em"}}/>
+                </div>
+                <button className={`${Styles.downbtn} mt-10`} onClick={downloadQRCode}>Download QR Code</button>
+            </div> */}
+    </div>
+  )
+}
+
+export default QrCodeGen;
+
+// export default function QrCodeGen({EventName}){
+//     // const qrCodeRef = useRef(null);
+//     // const downloadQRCode = () => {
+//     //     const svg = qrCodeRef.current.querySelector('svg');
+//     //     const svgData = new XMLSerializer().serializeToString(svg);
+//     //     const blob = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' });
+//     //     const url = URL.createObjectURL(blob);
+
+//     //     const link = document.createElement('a');
+//     //     link.href = url;
+//     //     link.download = 'qr-code.svg';
+//     //     document.body.appendChild(link);
+//     //     link.click();
+//     //     document.body.removeChild(link);
+//     //     URL.revokeObjectURL(url);
+//     // };
+//     return(
+//         <div className={Styles.QrCodeGen}>
+//             hii
+//             {/* <div>
+//                 <div style={{ color: 'black', fontSize: '20px' }}>Scan QR code to upload your selfie</div>
+//                 <div ref={qrCodeRef} style={{ boxShadow: '0px 12px 30px 0px rgba(0, 0, 0, 0.8)' }}>
+//                     <QRCodeSVG value={`${process.env.NEXT_PUBLIC_WEB_APP_BASE_URL}/upload/${EventName}`} style={{width:"18em",height:"18em",margin:"1em"}}/>
+//                 </div>
+//                 <button className={`${Styles.downbtn} mt-10`} onClick={downloadQRCode}>Download QR Code</button>
+//             </div> */}
+//         </div>
+//     )
+// }
