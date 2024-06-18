63bcaa8c92332fb1aeae7e68574b4c002d26ecdf
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-06-18 14:03:30 +0530

    Download Opt

--- app/crm/Components/Home/DownloadCSV.js
+++ app/crm/Components/Home/DownloadCSV.js
@@ -1,4 +1,9 @@
+import * as XLSX from 'xlsx';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
 export default function downloadCSV(data) {
+    console.log(data)
     if(data.length != 0){
     function convertJSONToCSV() {
         const header = Object.keys(data[0]).join(',');
@@ -13,6 +18,39 @@ export default function downloadCSV(data) {
     downloadLink.click();
     }
 }
+
+export function downloadEXCEL(data) {
+  console.log(data)
+  if(data.length != 0){
+
+  const worksheet = XLSX.utils.json_to_sheet(data);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+  const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  const blob_ = new Blob([excelBuffer], { type: 'application/octet-stream' });
+  const downloadLink_ = document.createElement('a');
+  downloadLink_.href = window.URL.createObjectURL(blob_);
+  downloadLink_.setAttribute('download', 'Excel_Data.xlsx');
+  downloadLink_.click();
+  }
+}
+
+export function downloadPDF(data) {
+  console.log(data)
+  if(data.length != 0){
+
+  const doc = new jsPDF();
+  const columns = Object.keys(data[0]);
+  const rows = data.map(obj => columns.map(col => obj[col]));
+  doc.autoTable({
+      head: [columns],
+      body: rows
+  });
+  doc.save('PDF_Data.pdf');
+  }
+}
+
+
 export function searchFun(query, SetData) {
     const searchTerm = query.toLowerCase();
     const results = SetData.filter(item => {
--- app/crm/Components/Home/EditDrawer.jsx
+++ app/crm/Components/Home/EditDrawer.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Style from "./edit.module.css"
-import downloadCSV,{searchFun} from './DownloadCSV';
+import downloadCSV,{downloadEXCEL,downloadPDF,searchFun} from './DownloadCSV';
 import EditCreate from './EditCreate';
 import EventDetailsToDownload from './EventDetailstoDownload';
 import { DeleteCustomerFuntion, GetCustomerFuntion } from './AllFunctions';
@@ -52,6 +52,24 @@ export default function EditLeftDrawer() {
     FetchCustomerData();
   },[]);
 
+  const [dropdownVisible, setDropdownVisible] = React.useState(false);
+  const handleDownload = (format) => {
+      switch (format) {
+          case 'csv':
+              downloadCSV(Data);
+              break;
+          case 'excel':
+              downloadEXCEL(Data);
+              break;
+          case 'pdf':
+              downloadPDF(Data);
+              break;
+          default:
+              break;
+      }
+      setDropdownVisible(false);
+  };
+
   const list = (anchor) => (
     <Box className={`${Style.DrawerCenter} min-h-screen`} style={{backgroundColor:"var(--bg)"}} sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : '100vw' }} role="presentation">
         <div className='w-10/12 m-auto flex flex-col pt-4 gap-4 min-h-screen' style={{width:'80%',margin:"auto"}}>
@@ -70,13 +88,33 @@ export default function EditLeftDrawer() {
 
             {/* Search */}
             <div className={`${Style.NavSearchModel} mb-4`}>
-                {/* <div className={Style.ForBackButton} onClick={()=>{setState({ ...state, [anchor]: false })}}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_263_943)"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.94063 13.0599C7.65973 12.7787 7.50195 12.3974 7.50195 11.9999C7.50195 11.6024 7.65973 11.2212 7.94063 10.9399L13.5966 5.2819C13.878 5.00064 14.2596 4.84268 14.6575 4.84277C14.8545 4.84282 15.0495 4.88167 15.2315 4.9571C15.4135 5.03253 15.5789 5.14307 15.7181 5.2824C15.8574 5.42173 15.9679 5.58713 16.0432 5.76915C16.1186 5.95117 16.1573 6.14625 16.1573 6.34325C16.1572 6.54025 16.1184 6.73531 16.0429 6.9173C15.9675 7.09929 15.857 7.26463 15.7176 7.4039L11.1226 11.9999L15.7186 16.5959C15.862 16.7342 15.9763 16.8997 16.055 17.0826C16.1337 17.2656 16.1752 17.4624 16.177 17.6616C16.1788 17.8607 16.141 18.0583 16.0656 18.2427C15.9903 18.427 15.879 18.5946 15.7382 18.7355C15.5975 18.8764 15.43 18.9878 15.2457 19.0633C15.0614 19.1389 14.8639 19.1769 14.6647 19.1753C14.4656 19.1736 14.2687 19.1323 14.0857 19.0538C13.9026 18.9753 13.7371 18.8611 13.5986 18.7179L7.93863 13.0599H7.94063Z" fill="white"/></g><defs><clipPath id="clip0_263_943"><rect width="24" height="24" fill="white"/></clipPath></defs></svg></div> */}
                 <div className={Style.SearchModel}>
                     <input type="text" placeholder="Search" onChange={(e)=>{SetData(searchFun(e.target.value,ConstData))}}/>
                     <div className={Style.searchBtn}>Search</div>
                 </div>
                 <div className={Style.AddModelButton}>
-                  <button  className={Style.searchBtn} style={{border:"1px solid var(--pink)",color:"var(--pink)",backgroundColor:"var(--white)"}} onClick={()=>{downloadCSV(Data)}}>Download as CSV</button>
+                  <div style={{ position: 'relative', display: 'inline-block' }}>
+            <button 
+                className={Style.searchBtn} 
+                style={{ display:"flex", gap:"1em",alignItems:"center",justifyContent:"space-between",border: "1px solid var(--pink)", color: "var(--pink)", backgroundColor: "var(--bg)" }}
+                onClick={() => setDropdownVisible(!dropdownVisible)} 
+            >
+                Download as <img src="/assets/downarr.svg" alt="Down Arrow" />
+            </button>
+            {dropdownVisible && (
+                <div className={`${Style.dropdown} flex flex-col py-2`} style={{ position: 'absolute', top: '100%', left: "80px", zIndex: 1 }}>
+                    <div style={{borderBottom:"1px solid black",width:"4em"}}>
+                      <button style={{padding:"0em .7em"}} onClick={() => handleDownload('csv')}>CSV</button>
+                    </div>
+                    <div style={{borderBottom:"1px solid black",width:"4em"}}>
+                      <button style={{padding:"0em .7em"}} onClick={() => handleDownload('excel')}>EXCEL</button>
+                    </div>
+                    <div style={{borderBottom:"1px solid black",width:"4em"}} >
+                      <button style={{padding:"0em .7em"}} onClick={() => handleDownload('pdf')}>PDF</button>
+                    </div>
+                </div>
+            )}
+        </div>
                   <EditCreate FetchCustomerData={FetchCustomerData}/>
                 </div>  
             </div>
