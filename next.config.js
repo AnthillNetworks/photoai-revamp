a74bdcb06411af86d831e135bd00fdd0ca60168e
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-06-27 20:58:07 +0530

    Size ..

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
         unoptimized : true,
         domains: ['mui.com','selife-bucket.s3.ap-south-1.amazonaws.com'],
     },
-}
+    serverActions: {
+        bodySizeLimit: '20mb' // Set desired value here
+    }
 
+}
 module.exports = nextConfig
